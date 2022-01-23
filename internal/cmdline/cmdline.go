259724f6a99272edfb8fa2f8334127023b2246ea
Richard Maxwell <richard@rsmaxwell.co.uk>
2022-01-23 17:45:53 +0000

    rename to jobdsl-to-xml

--- internal/cmdline/cmdline.go
+++ internal/cmdline/cmdline.go
@@ -2,7 +2,10 @@ package cmdline
 
 import (
 	"flag"
+	"fmt"
 	"os"
+
+	"github.com/rsmaxwell/job-to-xml/internal/basic"
 )
 
 // Config type
@@ -15,6 +18,8 @@ func GetArguments() (CommandlineArguments, error) {
 
 	help := flag.Bool("help", false, "Display help text")
 
+	version := flag.Bool("version", false, "Display version")
+
 	dsl := flag.String("dsl", "", "Input DSL filename")
 
 	xml := flag.String("xml", "", "Output XML filename")
@@ -22,7 +27,18 @@ func GetArguments() (CommandlineArguments, error) {
 	flag.Parse()
 
 	if *help {
+		fmt.Printf("job-to-xml: Convert jenkins job dsl to the xml configuration for a SEED job\n")
 		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	if *version {
+		fmt.Printf("job-to-xml: Convert jenkins job dsl to the xml configuration for a SEED job\n")
+		fmt.Printf("    Version:   %s\n", basic.Version())
+		fmt.Printf("    BuildDate: %s\n", basic.BuildDate())
+		fmt.Printf("    GitCommit: %s\n", basic.GitCommit())
+		fmt.Printf("    GitBranch: %s\n", basic.GitBranch())
+		fmt.Printf("    GitURL:    %s\n", basic.GitURL())
 		os.Exit(0)
 	}
 
